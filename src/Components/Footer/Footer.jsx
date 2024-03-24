7ac71a91018003347a05752a8e8a2b340b43fa1a
Nurmira16 <nurmirajorobek@gmail.com>
2024-03-24 15:31:54 +0100

    Implemented and designed lounge, subscribers footer part of web application
    This commit implements and designs the lounge and subscribers footer section of the web application. The lounge section provides a comfortable and visually appealing space for users to relax and explore content. Additionally, the subscribers footer is designed to attract users to subscribe to the applications newsletter or updates, enhancing user engagement and retention.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
-
+import { AiFillYoutube, AiOutlineTwitter } from 'react-icons/ai';
+import {TiSocialFacebook} from "react-icons/ti";
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='sectionContainer container grid'>
+      <div className='gridOne'>
+        <div className='logoDiv'>
+          <img src="src/assets/imgLogo.png" alt="" />
+        </div>
+        <p>Your mind should be stronger than your feelings, fly!</p>
+      </div>
+      <div className='socialIcon flex'>
+        <TiSocialFacebook className='icon'/>
+        <AiOutlineTwitter className='icon'/>
+        <AiFillYoutube className='icon'/>
+      </div>
+      <div className='footerLinks'>
+        <span className='linkTitle'>Information</span>
+        <li><a href="">Home</a></li>
+        <li><a href="">Explore</a></li>
+        <li><a href="">Flight Status</a></li>
+        <li><a href="">Travel</a></li>
+        <li><a href="">Check-In</a></li>
+        <li><a href="">Manage your booking</a></li>
+      </div>
+      <div className='footerLinks'>
+        <span className='linkTitle'>Quick Guide</span>
+        <li><a href="">FAQ</a></li>
+        <li><a href="">How To</a></li>
+        <li><a href="">Features</a></li>
+        <li><a href="">Baggage</a></li>
+        <li><a href="">Route Map</a></li>
+        <li><a href="">Our Communities</a></li>
+      </div>
+      <div className='footerLinks'>
+        <span className='linkTitle'>Quick Guide</span>
+        <li><a href="">Chauffuer</a></li>
+        <li><a href="">Our Partners</a></li>
+        <li><a href="">Destination</a></li>
+        <li><a href="">Careers</a></li>
+        <li><a href="">Transportation</a></li>
+        <li><a href="">Program Rules</a></li>
+      </div>
     </div>
+  
   );
 }
 
--- src/Components/Travelers/Travelers.jsx
+++ src/Components/Travelers/Travelers.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
 
 const Travelers = () => {
+
+  const travelers=[
+    {
+      id:1,
+      destinationImage:'src/assets/imgBcg.jpeg',
+      travelerImage:'src/assets/user1.jpg',
+      travelerName:'IsraTech',
+      socialLink:'isratech0'
+    },
+    {
+      id:2,
+      destinationImage:'src/assets/imgDst2.jpeg',
+      travelerImage:'src/assets/user2.jpg',
+      travelerName:'Brian',
+      socialLink:'brian22'
+    },
+    {
+      id:3,
+      destinationImage:'src/assets/imgDst4.jpeg',
+      travelerImage:'src/assets/user3.jpg',
+      travelerName:'Lina',
+      socialLink:'linaray1'
+    }
+  ]
   return (
-    <div>
+    <div className='travelers container section'>
+      <div className='sectionContainer'>
+        <h2> Top travelers of this month!</h2>
+
+        <div className='travelersContainer grid'>
+          {travelers.map(({id,destinationImage,travelerImage,travelerName,socialLink})=>{
+            return (
+              <div key={id} className='singleTraveler'>
+            <img src={destinationImage} alt="" className='destinationImage'/>
+            <div className='travelerDetails'>
+              <div className='travelerPicture'>
+                <img src={travelerImage} className="travelerImage" />
+              </div>
+            </div>
+            <div className='travelerName'>
+              <span>{travelerName}</span>
+             <p> {socialLink}</p>
+            </div>
+          </div>
+            )
+          })}
+        </div>
+      </div>
       
     </div>
   );
